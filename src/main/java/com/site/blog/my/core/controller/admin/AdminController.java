a0fd0fe38054141763c871ba31d0ff6d5765ae35
Cola2333 <594545815@qq.com>
2020-05-19 17:15:23 -0700

    add admin login index and interceptor

--- src/main/java/com/site/blog/my/core/controller/admin/AdminController.java
+++ src/main/java/com/site/blog/my/core/controller/admin/AdminController.java
@@ -12,9 +12,6 @@ import javax.servlet.http.HttpSession;
 
 /**
  * @author 13
- * @qq交流群 796794009
- * @email 2449207463@qq.com
- * @link http://13blog.site
  */
 @Controller
 @RequestMapping("/admin")
